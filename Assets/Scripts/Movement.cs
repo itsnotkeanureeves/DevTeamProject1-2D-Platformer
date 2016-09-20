82f2c5b2eb41c99dd097aa3a0d59ea5cc7bbf1bc
keanuconcepcion <keanuconcepcion@gmail.com>
2016-09-19 18:27:23 -0700

    Transfe Unity project to Github

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	float Speed = 0.1f;
+	float jumpSpeed = 0.2f;
+
+	void Update () {
+		{
+			if (Input.GetKey("a") )
+			{
+				transform.position = new Vector2 (transform.position.x - Speed,transform.position.y);
+			}
+			if ( Input.GetKey("d") )
+			{
+				transform.position = new Vector2 (transform.position.x + Speed,transform.position.y);
+	}
+			if (Input.GetKey ("space")) {	
+				transform.position = new Vector2 (transform.position.x, transform.position.y + jumpSpeed);
+			}
+}
+	}
+}
